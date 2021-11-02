c1ab05d282141462077b3c8979e508d470df4b7c
It's Lex <lex@crema.us>
2021-11-01 19:49:37 -0500

    Add icon credit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const HomePage = () =>{
-    return <div> Welcome to Next.js! </div>
+    return <div>
+        <div> Welcome to Next.js! </div>
+        <div>Icons made by 
+            <a href="https://www.freepik.com" title="Freepik">Freepik</a> 
+            from 
+            <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com</a>
+        </div>
+    </div>
 }
 
 export default HomePage
