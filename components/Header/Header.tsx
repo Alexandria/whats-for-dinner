acebdb7f5388351afe785cee06ca6b4e9f66ab6e
It's Lex <lex@crema.us>
2021-11-05 13:36:53 -0500

    Add header and footer components

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () =>{
+    return <div className={styles.header}>
+        <div> Whats for dinner? </div>
+    </div>
+}
+
+export default Header
